62f12f73996ec2f7fe112f30dea811533aa64c30
Brad Kay <bradleytkay@Brads-MacBook-Air.local>
2024-08-07 13:30:25 -0500

    completed assingment in reading

--- hello.py
+++ hello.py
@@ -4,16 +4,40 @@
 # to the console (the panel on the right)
 
 print("Hello");
+print('Hello, World!');
 
 # Next, PLAY!
 # Modify your code on line 7 to try to accomplish the tasks listed below:
 
 #   1. Change the message that is printed.
+
+print('Goodbye, World!');
+
 #   2. Figure out what the parentheses do. Will the code work without them?
+
+# Code will not work without parentheses.
+
 #   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+
+# Need both opening and closing quotes.  Doesn't matter if ' or '' is used, as long as it's consistent.
+
 #   4. Print a number. (Bonus: Print two numbers added together).
+
+print('4');
+
 #   5. Print multiple messages one after the other.
+
+print('First message')
+print('Second message');
+
 #   6. Print two messages on the same line.
+
+print('First message,' 'Second message');
+
 #   7. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
+
+print('Quoth the Raven "Nevermore"');
+
 #   8. Other. You choose!
-#
+
+print('Thanks!  That was fun.')
